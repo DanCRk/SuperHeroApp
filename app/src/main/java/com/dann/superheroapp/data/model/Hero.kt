ccec576b5e651946336c5e944bdf4ab45e26dca7
DanCr <dancrkyt@gmail.com>
2022-03-08 22:42:10 -0600

    ViewModel del Main activity, recuperando la lista de heroes, y una segunda funcion para obtener los siguientees 20 heroes

--- app/src/main/java/com/dann/superheroapp/data/model/Hero.kt
+++ app/src/main/java/com/dann/superheroapp/data/model/Hero.kt
@@ -1,6 +1,9 @@
 package com.dann.superheroapp.data.model
 
+import java.io.Serializable
+
 data class Hero(
     val name:String,
+    val id:Int,
     val thumbnail:Thumbnail
-)
+):Serializable
---
+++ app/src/main/java/com/dann/superheroapp/ui/viewmodel/HeroesViewModel.kt
@@ -0,0 +1,43 @@
+package com.dann.superheroapp.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dann.superheroapp.data.model.Hero
+import com.dann.superheroapp.domain.GetAllHeroesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HeroesViewModel @Inject constructor(
+    private val getAllHeroes: GetAllHeroesUseCase
+) : ViewModel() {
+    val heroesViewModel = MutableLiveData<List<Hero>>()
+    val progressBar = MutableLiveData<Boolean>()
+    val loadingMoreHeroes = MutableLiveData<Boolean>()
+
+    fun onCreate(){
+        viewModelScope.launch {
+            progressBar.postValue(true)
+            val result = getAllHeroes("v1/public/characters?ts=1&limit=20&offset=0&apikey=6297766f747707d8733f11ec846ba65c&hash=9cae473e2dbf1e5c71aed13ab674a620")
+            if(!result.isNullOrEmpty()){
+                heroesViewModel.postValue(result)
+                progressBar.postValue(false)
+            }
+        }
+    }
+
+    fun loadMore(offset:Int){
+        viewModelScope.launch {
+            loadingMoreHeroes.postValue(true)
+            val result = getAllHeroes("v1/public/characters?ts=1&limit=20&offset=${offset}&apikey=6297766f747707d8733f11ec846ba65c&hash=9cae473e2dbf1e5c71aed13ab674a620")
+            if(!result.isNullOrEmpty()){
+                heroesViewModel.postValue(result)
+                loadingMoreHeroes.postValue(false)
+            }
+        }
+    }
+
+}
