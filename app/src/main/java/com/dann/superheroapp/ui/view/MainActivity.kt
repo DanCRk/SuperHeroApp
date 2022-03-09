d242b8d3809842504c104edfcec2d12bcf2d2eb4
DanCr <dancrkyt@gmail.com>
2022-03-08 18:49:28 -0600

    Organizacion y carpetas respectivas para el proyecto

--- app/src/main/java/com/dann/superheroapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/dann/superheroapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dann.superheroapp
+package com.dann.superheroapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dann.superheroapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
