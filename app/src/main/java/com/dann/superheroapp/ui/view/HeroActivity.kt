62822873b70d46d5d61337d078420ea0b7f0363d
DanCr <dancrkyt@gmail.com>
2022-03-09 18:11:41 -0600

    Progressbar para cuando se estan cargando mas wallpapers, y corregir un error con imagenes repetidas

--- app/src/main/java/com/dann/superheroapp/ui/view/HeroActivity.kt
+++ app/src/main/java/com/dann/superheroapp/ui/view/HeroActivity.kt
@@ -1,22 +1,24 @@
 package com.dann.superheroapp.ui.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import com.bumptech.glide.Glide
 import com.dann.superheroapp.R
+import com.dann.superheroapp.data.adapters.viewpager.SliderAdapter
 import com.dann.superheroapp.data.model.Hero
+import com.dann.superheroapp.data.model.Images
 import com.dann.superheroapp.databinding.ActivityHeroBinding
 import com.dann.superheroapp.ui.viewmodel.HeroDetailsViewModel
-import com.dann.superheroapp.data.adapters.viewpager.SliderAdapter
-import com.dann.superheroapp.data.model.Images
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.math.abs
 
@@ -33,7 +35,7 @@ class HeroActivity : AppCompatActivity() {
 
     private val comicImageList = mutableListOf<Images>()
 
-    private val heroViewModel : HeroDetailsViewModel by viewModels()
+    private val heroViewModel: HeroDetailsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,15 +48,17 @@ class HeroActivity : AppCompatActivity() {
 
         setInformationInViews()
 
-        heroViewModel.comicsViewModel.observe(this){
+        goToDetails()
+
+        heroViewModel.comicsViewModel.observe(this) {
             if (it.extension != "_") {
-                if (it.path.split("/").last() != "image_not_available"){
+                if (it.path.split("/").last() != "image_not_available") {
                     val url =
                         "${it.path}/standard_fantastic.${it.extension}".replace(
                             "http",
                             "https"
                         )
-                    val comicImage = Images(url, "",it.name)
+                    val comicImage = Images(url, "", it.name)
                     comicImageList.add(comicImage)
                     binding.noComics.isVisible = false
                     refresh()
@@ -63,8 +67,20 @@ class HeroActivity : AppCompatActivity() {
         }
     }
 
+    private fun goToDetails(){
+        binding.fav.setOnClickListener {
+            val uri = hero.urls.first().url
+            if(!uri.equals(null)){
+                val intent = Intent(Intent.ACTION_VIEW, uri.toUri())
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "No hay pagina web en este momento!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun setUpViewPager() {
-        adapter = SliderAdapter(comicImageList){onItemListener(it)}
+        adapter = SliderAdapter(comicImageList) { onItemListener(it) }
         binding.sliderComics.adapter = adapter
         binding.sliderComics.clipToPadding = false
         binding.sliderComics.clipChildren = false
@@ -73,7 +89,7 @@ class HeroActivity : AppCompatActivity() {
 
         val compositePagerTransformer = CompositePageTransformer()
         compositePagerTransformer.addTransformer(MarginPageTransformer(30))
-        compositePagerTransformer.addTransformer{page, position ->
+        compositePagerTransformer.addTransformer { page, position ->
             val r = 1 - abs(position)
             page.scaleY = 0.85f + r * 0.25f
         }
@@ -89,19 +105,24 @@ class HeroActivity : AppCompatActivity() {
         heroId = hero.id
         heroName = hero.name
         heroDescription = hero.description
-        heroUrl = "${hero.thumbnail.path}/standard_fantastic.${hero.thumbnail.extension}".replace(
-            "http",
-            "https"
-        )
-        for (comic in hero.comics.items){
-            val uri = "${comic.resourceURI}?ts=1&apikey=6297766f747707d8733f11ec846ba65c&hash=9cae473e2dbf1e5c71aed13ab674a620".replace("http","https")
+        heroUrl = hero.thumbnail.path
+        for (comic in hero.comics.items) {
+            val uri =
+                "${comic.resourceURI}?ts=1&apikey=6297766f747707d8733f11ec846ba65c&hash=9cae473e2dbf1e5c71aed13ab674a620".replace(
+                    "http",
+                    "https"
+                )
             heroViewModel.onCreate(uri)
         }
     }
 
     private fun setInformationInViews() {
-        if (heroUrl.split("/")[10] != "image_not_available") {
-            Glide.with(this).load(heroUrl).centerCrop().into(binding.imgHeroDetail)
+        if (heroUrl.split("/").last() != "image_not_available") {
+            val uri = "$heroUrl/standard_fantastic.${hero.thumbnail.extension}".replace(
+            "http",
+            "https"
+            )
+            Glide.with(this).load(uri).centerCrop().into(binding.imgHeroDetail)
         } else {
             binding.imgHeroDetail.setImageDrawable(
                 AppCompatResources.getDrawable(
@@ -118,5 +139,9 @@ class HeroActivity : AppCompatActivity() {
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    private fun refresh(){ if(!comicImageList.isNullOrEmpty()){ adapter.notifyDataSetChanged() } }
+    private fun refresh() {
+        if (!comicImageList.isNullOrEmpty()) {
+            adapter.notifyDataSetChanged()
+        }
+    }
 }
