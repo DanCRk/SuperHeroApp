070da0ecdb5be3176e63279dd748d6b5c8b2a979
DanCr <dancrkyt@gmail.com>
2022-03-09 19:09:10 -0600

    Movel la logica al caso de uso, para que el maneje como le llegan las imagenes

--- app/src/main/java/com/dann/superheroapp/data/Repository.kt
+++ app/src/main/java/com/dann/superheroapp/data/Repository.kt
@@ -7,19 +7,16 @@ import javax.inject.Inject
 
 class Repository @Inject constructor(private val apiClient: MarvelService) {
 
-    suspend fun getHeroes(url:String):List<Hero>{
+    suspend fun getHeroes(url: String): List<Hero> {
         val response = apiClient.getHeroes(url)
         return response.data.results
     }
 
-    suspend fun getImage(url:String):Images{
+    suspend fun getImage(url: String): Images {
         val response = apiClient.getHeroes(url)
-        return if (!response.data.results.first().images.isNullOrEmpty()){
-            response.data.results.first().images.first().name = response.data.results.first().title
-            response.data.results.first().images.first()
-        }else{
-            Images()
-        }
+        response.data.results.first().images.first().name = response.data.results.first().title
+        return response.data.results.first().images.first()
+
     }
 
 }
